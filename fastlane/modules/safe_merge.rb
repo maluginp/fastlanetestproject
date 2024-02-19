a1412f0612cf7e9c1830da4d1a38438eb0bff12f
Platon Malyugin <malugin.p@gmail.com>
2024-02-19 23:41:50 +0300

    Fixed safe merge

--- fastlane/modules/safe_merge.rb
+++ fastlane/modules/safe_merge.rb
@@ -3,8 +3,6 @@ require 'fastlane'
 # Safe merge for actual branch
 module SafeMerge
 
-  include Fastlane::Actions
-
   def self.main_branch?
     ENV['MAIN_BRANCH'] == fetch_local_branch
   end
@@ -23,16 +21,16 @@ module SafeMerge
       ]
 
       # execute our command
-      return command.join(' ') if Helper.test?
+      return command.join(' ') if Fastlane::Helper.test?
 
-      Actions.sh(command.join(' '))
-      UI.message("Successfully merged #{main_branch} (main branch) to #{local_git_branch}")
+      Fastlane::Actions.sh(command.join(' '))
+      Fastlane::UI.success("Successfully merged #{main_branch} (main branch) to #{local_git_branch}")
 
   end
 
   def self.fetch_local_branch
     local_git_branch = Fastlane::Actions.git_branch_name_using_HEAD
-    local_git_branch = git_branch unless local_git_branch && local_git_branch != 'HEAD'
+    local_git_branch = Fastlane::Actions.git_branch unless local_git_branch && local_git_branch != 'HEAD'
     local_git_branch
   end
 end
